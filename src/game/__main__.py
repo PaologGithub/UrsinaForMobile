ce4503781b7a5f65d9d40253b54cc5066b51257b
PaologGithub <PaologGithub>
2024-11-24 16:38:03 +0100

    Changed __main__.py

--- src/game/__main__.py
+++ src/game/__main__.py
@@ -4,6 +4,12 @@ setup_ursina_android()
 
 from ursina import *
 
+# If you want, you can use this method to destroy camera.overlay, which will show the background of the app.
+# It is because camera.overlay is transparent, but transparency doesn't work on this project.
+# So for now, you can use this to help you
+""" from ursina.ursinastuff import _destroy
+_destroy(camera.overlay, True)
+camera.overlay = Entity() """
 
 # Initialize ursina app
 app = Ursina()
@@ -11,20 +17,13 @@ app = Ursina()
 # Window setttings
 window.exit_button.enabled = False
 
-# Mesh from cube.ursinamesh
-""" cubeMesh = Mesh(
-    vertices=((0.5, -0.5, -0.5), (-0.5, -0.5, 0.5), (0.5, -0.5, 0.5), (-0.5, 0.5, 0.5), (0.5, 0.5, -0.5), (0.5, 0.5, 0.5), (0.5, 0.5, 0.5), (0.5, -0.5, -0.5), (0.5, -0.5, 0.5), (0.5, 0.5, -0.5), (-0.5, -0.5, -0.5), (0.5, -0.5, -0.5), (-0.5, -0.5, -0.5), (-0.5, 0.5, 0.5), (-0.5, -0.5, 0.5), (0.5, -0.5, 0.5), (-0.5, 0.5, 0.5), (0.5, 0.5, 0.5), (0.5, -0.5, -0.5), (-0.5, -0.5, -0.5), (-0.5, -0.5, 0.5), (-0.5, 0.5, 0.5), (-0.5, 0.5, -0.5), (0.5, 0.5, -0.5), (0.5, 0.5, 0.5), (0.5, 0.5, -0.5), (0.5, -0.5, -0.5), (0.5, 0.5, -0.5), (-0.5, 0.5, -0.5), (-0.5, -0.5, -0.5), (-0.5, -0.5, -0.5), (-0.5, 0.5, -0.5), (-0.5, 0.5, 0.5), (0.5, -0.5, 0.5), (-0.5, -0.5, 0.5), (-0.5, 0.5, 0.5)), 
-    uvs=((1.0, 0.0), (0.0, 1.0), (1.0, 1.0), (0.0, 1.0), (1.0, -0.0), (1.0, 1.0), (1.0, 1.0), (0.0, 0.0), (1.0, 0.0), (1.0, 1.0), (-0.0, 0.0), (1.0, 0.0), (-0.0, 0.0), (1.0, 1.0), (1.0, 0.0), (1.0, 0.0), (0.0, 1.0), (1.0, 1.0), (1.0, 0.0), (-0.0, 0.0), (0.0, 1.0), (0.0, 1.0), (-0.0, 0.0), (1.0, -0.0), (1.0, 1.0), (-0.0, 1.0), (0.0, 0.0), (1.0, 1.0), (-0.0, 1.0), (-0.0, 0.0), (-0.0, 0.0), (-0.0, 1.0), (1.0, 1.0), (1.0, 0.0), (0.0, 0.0), (0.0, 1.0)), 
-    normals=[(-0.0, -1.0, 0.0), (-0.0, -1.0, 0.0), (-0.0, -1.0, 0.0), (-0.0, 1.0, -0.0), (-0.0, 1.0, -0.0), (-0.0, 1.0, -0.0), (1.0, -0.0, 0.0), (1.0, -0.0, 0.0), (1.0, -0.0, 0.0), (-0.0, -0.0, -1.0), (-0.0, -0.0, -1.0), (-0.0, -0.0, -1.0), (-1.0, -0.0, 0.0), (-1.0, -0.0, 0.0), (-1.0, -0.0, 0.0), (0.0, 0.0, 1.0), (0.0, 0.0, 1.0), (0.0, 0.0, 1.0), (-0.0, -1.0, 0.0), (-0.0, -1.0, 0.0), (-0.0, -1.0, 0.0), (-0.0, 1.0, -0.0), (-0.0, 1.0, -0.0), (-0.0, 1.0, -0.0), (1.0, -0.0, 0.0), (1.0, -0.0, 0.0), (1.0, -0.0, 0.0), (-0.0, -0.0, -1.0), (-0.0, -0.0, -1.0), (-0.0, -0.0, -1.0), (-1.0, -0.0, 0.0), (-1.0, -0.0, 0.0), (-1.0, -0.0, 0.0), (0.0, 0.0, 1.0), (0.0, 0.0, 1.0), (0.0, 0.0, 1.0)], 
-    mode='triangle'
-) """
 # Widget
 Text("Ursina on Android", scale=2, position=(.0, .3), origin=(0, 0))
+Entity(model="quad.ursinamesh", scale=.1, position=(.0, .2), origin=(0, 0), texture="ursina", parent=camera.ui)
 Text("It finally works", position=(.0, .1), origin=(0, 0))
 
 welcomeText = Text("Hello, World!", position=(0, 0), scale=1.5, origin=(0, 0))
 cube = Entity(model="cube.ursinamesh", scale=.1, position=(0, -.1), origin=(0, 0), color=color.red, parent=camera.ui)
-Entity(model="quad.ursinamesh", scale=.1, position=(.0, .3), origin=(0, 0), texture="ursina", parent=camera.ui)
 button = Button("Change background color", color=color.lime, pressed_color=color.yellow, origin=(0, 0), position=(0, -.3), scale=(.4, .07), text_color=color.gray)
 
 # Change window color to random color on button click
